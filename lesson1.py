cf5e112e9b3b857baeea1568971ec76f2e4fddbd
Solwar <sacredglad@gmail.com>
2020-04-30 20:17:24 +0300

    first commit

---
+++ lesson1.py
@@ -0,0 +1,9 @@
+def weather(temperature):
+    if temperature < 0:
+        return 'So cold'
+    elif 0 <= temperature <= 15:
+        return 'A little cold'
+    elif 15 <= temperature <= 25:
+        return 'A temperature is normal'
+    else:
+        return 'Hot!'
