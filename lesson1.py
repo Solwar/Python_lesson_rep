c0278d14d499ea6a2e9dce164e0e5a662663ffe5
Solwar <sacredglad@gmail.com>
2020-05-01 20:31:39 +0300

    3rd version with updated discount def

--- lesson1.py
+++ lesson1.py
@@ -8,5 +8,17 @@ def weather(temperature):
     else:
         return 'Hot!'
 
+def discounted(price, discount, max_discount=20, name=''):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    max_discount = abs(float(max_discount))
+    if max_discount > 99:
+        raise ValueError('Слишком большая максимальная скидка')
+    if discount >= max_discount or "iphone" in name.lower():
+        return price
+    else:
+        return price - (price * discount / 100)
 
-print(weather(5))
+print(discounted(50000, 10, name='OPPO'))
+
+#print(weather(5))
