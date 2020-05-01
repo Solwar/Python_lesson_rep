182a837665206969842c14f78cfed509cc305c69
Solwar <sacredglad@gmail.com>
2020-05-01 20:39:40 +0300

    4 version with updated discount def

--- lesson1.py
+++ lesson1.py
@@ -14,11 +14,10 @@ def discounted(price, discount, max_discount=20, name=''):
     max_discount = abs(float(max_discount))
     if max_discount > 99:
         raise ValueError('Слишком большая максимальная скидка')
-    if discount >= max_discount or "iphone" in name.lower():
+    if discount >= max_discount or "iphone" in name.lower() or not name:
         return price
     else:
         return price - (price * discount / 100)
-
 
 phone1 = {'name': 'iPhone Xs Plus', 'stock': 24, 'price': 65432.1, 'discount': 25}
 phone2 = {'name': 'Samsung Galaxy S10', 'stock': 8, 'price': 50000.0, 'discount': 10}
@@ -31,6 +30,8 @@ print(dis_iphone)
 dis_android = discounted(phone2['price'], phone2['discount'], name=phone2['name'])
 print(dis_android)
 
+dis_noname  = discounted(100, 10, name="")
+print(dis_noname)
 #print(discounted(50000, 10, name='OPPO'))
 
 #print(weather(5))
